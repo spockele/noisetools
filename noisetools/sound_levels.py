eb56d62957ea3882bf9c7923e47e5a517e18f13f
Josephine Pockele <j.s.pockele@tudelft.nl>
2024-03-06 10:11:26 +0100

    Split the equivalent pressure from the ospl function to allow for its separate determination

--- noisetools/sound_levels.py
+++ noisetools/sound_levels.py
@@ -6,8 +6,46 @@ import numpy as np
 from .weighting_functions import weigh_signal
 
 
-__all__ = ['ospl',
+__all__ = ['equivalent_pressure',
+           'ospl',
            ]
+
+
+def equivalent_pressure(signal: list | np.ndarray, fs: int | float | np.number, weighting: str = None) -> float:
+    """
+    Calculate the equivalent pressure (Pe^2) of the input sound signal.
+
+    Parameters
+    ----------
+    signal: array_like
+        Array with the digital signal.
+    fs: number
+        The sampling frequency of the digital signal
+    weighting: str, optional
+        The name of the optional weighting curve to be used. Can be 'A' or 'C'.
+
+    Returns
+    -------
+    float
+        Equivalent pressure in Pa^2 (weighted to selected weighting)
+    """
+    # Convert signal to numpy array
+    if not isinstance(signal, np.ndarray):
+        signal = np.array(signal)
+
+    # Check that this signal is 1d array
+    if len(signal.shape) > 1:
+        raise ValueError('noisetools.ospl only supports 1d signal arrays.')
+
+    # Apply the selected weighting
+    if weighting is not None:
+        signal = weigh_signal(signal, fs, curve=weighting)
+
+    # Determine the corresponding time for proper integration
+    t = np.linspace(0, signal.size / fs, signal.size)
+
+    # Determine the equivalent pressure
+    return 1 / t[-1] * np.trapz(signal ** 2, t)
 
 
 def ospl(signal: list | np.ndarray, fs: int | float | np.number, weighting: str = None) -> float:
@@ -28,22 +66,6 @@ def ospl(signal: list | np.ndarray, fs: int | float | np.number, weighting: str 
     float
         Overall sound pressure level in dB (weighted to selected weighting)
     """
-    # Convert signal to numpy array
-    if not isinstance(signal, np.ndarray):
-        signal = np.array(signal)
-
-    # Check that this signal is 1d array
-    if len(signal.shape) > 1:
-        raise ValueError('noisetools.ospl only supports 1d signal arrays.')
-
-    # Apply the selected weighting
-    if weighting is not None:
-        signal = weigh_signal(signal, fs, curve=weighting)
-
-    # Determine the corresponding time for proper integration
-    t = np.linspace(0, signal.size / fs, signal.size)
-
-    # Determine the equivalent pressure
-    pe2 = 1 / t[-1] * np.trapz(signal ** 2, t)
+    pe2 = equivalent_pressure(signal, fs, weighting)
 
     return 10 * np.log10(pe2 / (2e-5 ** 2))
