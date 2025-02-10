d8335e98223a9ba0f35daf079647ac3334bddfaf
Josephine Pockele <j.s.pockele@tudelft.nl>
2025-02-10 10:53:19 +0100

    Removed SQAT v1.1 from repository

--- noisetools/pysqat/SQAT-1.1/utilities/basepath_SQAT.m
+++
@@ -1,9 +0,0 @@
-function bp = basepath_SQAT
-% function bp = basepath_SQAT
-%
-% It returns the main (base) path of the toolbox.
-%
-% Author: Alejandro Osses
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-bp = [fileparts(fileparts(mfilename('fullpath'))) filesep];
